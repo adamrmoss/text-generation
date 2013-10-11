69ee72a05c70a110a61ce22672fb5311041288f9
Adam Moss <adamrmoss@me.com>
2013-10-11 10:53:16 -0700

    Created empty Leiningen project

---
+++ src/text_generation/core.clj
@@ -0,0 +1,6 @@
+(ns text-generation.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/text_generation/core_test.clj
@@ -0,0 +1,7 @@
+(ns text-generation.core-test
+  (:use clojure.test
+        text-generation.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
