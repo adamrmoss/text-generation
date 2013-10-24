520e8ed6aea7abe02946ef5ad4d8061de41bc00c
Adam Moss <adamrmoss@me.com>
2013-10-24 15:05:03 -0700

    Extracted distribution-to-choice-vector function

--- src/adamrmoss/random/core.clj
+++ src/adamrmoss/random/core.clj
@@ -1,11 +1,15 @@
 (ns adamrmoss.random.core)
 
+(defn distribution-entry-to-choice-vector [choice]
+  (repeat (val choice) (key choice)))
+
+(defn distribution-to-choice-vector [choices]
+  (mapcat distribution-entry-to-choice-vector choices))
+
 (defn choose [choices random]
   "Use a random number generator to choose an element"
   (cond
-    (map? choices) (let [distribution-entry-to-choice-array #(repeat (val %1) (key %1))
-                         expanded-array (mapcat distribution-entry-to-choice-array choices)]
-                     (choose expanded-array random))
+    (map? choices) (choose (distribution-to-choice-vector choices) random)
     (coll? choices) (let [choices-count (count choices)
                           random-index (.nextInt random choices-count)]
                       (nth choices random-index))))
