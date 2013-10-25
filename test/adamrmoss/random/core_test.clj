41ee158911c7e82f96219e7639913347853274d5
Adam Moss <adamrmoss@me.com>
2013-10-24 19:05:06 -0700

    minor

--- test/adamrmoss/random/core_test.clj
+++ test/adamrmoss/random/core_test.clj
@@ -5,7 +5,7 @@
 (deftest choices-from-collection
   (let [sample-vector [1, 2, 3, 4, 5]
         random (java.util.Random. 4298)]
-    (testing "Experimenting with some choices from [1, 2, 3, 4, 5]"
+    (testing "Making random choices from [1, 2, 3, 4, 5]"
       (is (= 3 (choose sample-vector random)))
       (is (= 2 (choose sample-vector random)))
       (is (= 5 (choose sample-vector random)))
@@ -21,7 +21,7 @@
 (deftest choices-from-distribution
   (let [sample-map {1 1, 2 5, 3 1}
         random (java.util.Random. 8711)]
-    (testing "Experimenting with some choices from {1 1, 2 5, 3 1}"
+    (testing "Making random choices using the distribution {1 1, 2 5, 3 1}"
       (is (= 2 (choose sample-map random)))
       (is (= 1 (choose sample-map random)))
       (is (= 2 (choose sample-map random)))
